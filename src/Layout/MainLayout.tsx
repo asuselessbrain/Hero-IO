bc1b25e5650309106c4ec3c822532f57df9791f3
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-10-07 22:18:50 +0600

    Add: Footer Design

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router";
 import NavBar from "../components/shared/NavBar";
+import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <NavBar />
-            <Outlet />
+            <div className="min-h-[calc(100vh-302px)]">
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,30 @@
+import { FaLinkedin } from "react-icons/fa";
+import logo from "../../assets/images/logo.png";
+import { FaSquareFacebook, FaSquareXTwitter } from "react-icons/fa6";
+const Footer = () => {
+    return (
+        <footer className="footer bg-[#001931]">
+            <div className="max-w-[1440px] mx-auto py-10 text-white w-full">
+                <div className="flex items-center justify-between w-full border-b-2 border-gray-600 pb-6 mb-6">
+                    <div className="flex items-center gap-2">
+                        <img src={logo} className="h-16 w-16" alt="" />
+                        <p className="font-bold text-xl">
+                            HERO.IO
+                        </p>
+                    </div>
+                    <div>
+                        <p className="text-[20px]">Social Links</p>
+                        <div className="flex gap-2 items-center mt-4">
+                            <FaLinkedin size={24} />
+                            <FaSquareXTwitter size={24} />
+                            <FaSquareFacebook size={24} />
+                        </div>
+                    </div>
+                </div>
+                <p  className="flex items-center justify-center w-full">Copyright &copy; {new Date().getFullYear()} - All right reserved by HERO.IO</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
